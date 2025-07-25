915ea93e02cd2cfa5e31979288beb45d9d5f5248
Femi <fagbemibabs18@gmail.com>
2025-07-25 01:52:27 +0100

    Initial commit with full res MovieCards and search bar

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const MovieCard = ({ movie: { title, poster_path, release_date, vote_average, original_language } }) => {
+  return (
+    <div className='movie-card'>
+        <img 
+        src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : "/no-movie.png"} 
+        alt={title} 
+        />
+        
+        <div className="mt-4">
+            <h3 className='text-white'>{title}</h3>
+
+            <div className="content">
+                <div className="rating">
+                    <img src="star.svg" alt="Star Rating" />
+                    <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+                </div>
+
+                <span>•</span>
+                <p className="lang"> {original_language}</p>
+
+                <span>•</span>
+                <p className="year">{release_date ? new Date(release_date).getFullYear() : "N/A"}</p>
+
+            </div>
+            {/* <p className='text-gray-400'>{release_date}</p>
+            <p className='text-yellow-500'>{vote_average}</p>
+            <p className='text-gray-400'>{original_language}</p> */}
+        </div>
+        
+    </div>
+  )
+}
+
+export default MovieCard
